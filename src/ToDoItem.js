f625a3eba36e20193bf6a373cbb06f17bb1d42e6
peter gilkey <peter_gilkey@yahoo.com>
2020-08-10 05:49:17 -0500

    todo app

---
+++ src/ToDoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ToDoItem = props => {
+  return(
+    <div>
+      <li>{props.listValue}</li>
+      <button onClick={props.deleteButton}>Delete</button>
+    </div>
+  )
+}
+
+export default ToDoItem
